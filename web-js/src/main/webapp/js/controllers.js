a8161b6aece4590aca70665c165d42ba5051bc42
Stian Thorgersen <stianst@gmail.com>
2013-02-01 13:16:12 +0000

    Fixed search events, added infinite scroll to events list

--- web-js/src/main/webapp/js/controllers.js
+++ web-js/src/main/webapp/js/controllers.js
@@ -1,15 +1,58 @@
 'use strict';
 
 function EventListCtrl($scope, Event, $routeParams) {
-    $scope.events = Event.query($routeParams);
+    var first = 0;
+    var max = 10;
+    var loading = false;
+
+    $scope.events = [];
+
+    $scope.loadMore = function() {
+        if (loading)
+            return;
+
+        loading = true;
+
+        var parameters = {
+            "first" : first,
+            "max" : max,
+            "sort" : "time"
+        };
+        
+        if ( $routeParams.query ) {
+            parameters.query = $routeParams.query;
+        }
+
+        Event.query(parameters, function(data) {
+            if (data.length == 0) {
+                return;
+            }
+
+            $scope.events = $scope.events.concat(data);
+            first += max;
+            loading = false;
+
+            $scope.$watch('events', function() {
+                if ($("body").height() < $(window).height()) {
+                    $scope.loadMore();
+                }
+            });
+        });
+    };
+
+    $scope.loadMore();
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            $scope.loadMore();
+        }
+    });
 }
 
 function EventSearchCtrl($scope, Event, $location) {
-    $scope.query = "",
-        
     $scope.search = function() {
         $location.url("/events?query=" + $scope.query);
-        $scope.query = "";
+        delete $scope.query;
     };
 }
 
